c857d890b1b6ef605f5c1a88f59e8811ec4d06cd
maikor Adonys inirio guerrero <adonisrddd@gmail.com>
2026-01-27 20:19:47 -0400

    Agregar archivos de proyecto.

---
+++ ISW123-ReporteroDigital-2023-3679/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ReporteroDigital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("📰 PERIÓDICO DIGITAL DOMINICANO");
+            Console.WriteLine("Desarrollador: Maikor Adonys Inirio Guerrero");
+            Console.WriteLine("Matrícula: 2023-3679");
+            Console.WriteLine("\nIniciando sistema...");
+            Console.ReadKey();
+        }
+    }
+}
